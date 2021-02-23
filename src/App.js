550f58b06480da49ce665aaed2f3a25137e348f9
Farah Laptop <153158+furii@users.noreply.github.com>
2021-02-23 19:34:49 +0100

    added Firebase Auth users to our Apps Firestore db when they login to app

--- src/App.js
+++ src/App.js
@@ -5,45 +5,42 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import Header from './components/header/header.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
-  
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
       currentUser: null
-    }
+    };
   }
 
-  unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
 
-  componentDidMount(){
-    auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user});
-
-      console.log(user)
-    })
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      createUserProfileDocument(user);
+    });
   }
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
-  
+
   render() {
     return (
-    <div>
-      <Header currentUser= {this.state.currentUser}/>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
-        <Route exact path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-    )
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
   }
 }
 
