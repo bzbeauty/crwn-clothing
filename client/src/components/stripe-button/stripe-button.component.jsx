c474ba4ff830a8b64f79a4cd7b8f0c070431031a
Farah Krecinic <farahkrecinic@gmail.com>
2021-04-23 13:51:22 +0200

    remove gitignore files

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51IVD9fAzehvykSUrww7xBR8IvNFZHQADScPdh92LwbzSKGWm0zsSzkBXnzuqQGmoWO4xkERDj6Rq4aoxVK3x9Pwt00suI0GhhI';
 
-const onToken = token => {
-    console.log(token);
-    alert('Payment Successful');
-}
+    const onToken = token => {
+        axios({
+          url: 'payment',
+          method: 'post',
+          data: {
+            amount: priceForStripe,
+            token: token
+          }
+        })
+          .then(response => {
+            alert('succesful payment');
+          })
+          .catch(error => {
+            console.log('Payment Error: ', error);
+            alert(
+              'There was an issue with your payment! Please make sure you use the provided credit card.'
+            );
+          });
+      };
 
     return (
         <StripeCheckout
