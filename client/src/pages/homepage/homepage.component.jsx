5dd4676d8eafbe186a9a8cbcd80423ce5af5377d
Farah Krecinic <farahkrecinic@gmail.com>
2021-05-06 15:07:35 +0200

    270. added error handling 404 page

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { HomePageContainer } from './homepage.styles';
 import Directory from '../../components/directory/directory.component';
 
-const HomePage = () => (
+const HomePage = () =>(
     <HomePageContainer>
         <Directory />
     </HomePageContainer>
